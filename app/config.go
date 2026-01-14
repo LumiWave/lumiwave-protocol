80c02087d38676a511d903caf98ace9d5e538b52
Top-Yone <thickeyes@gmail.com>
2026-01-14 10:46:47 +0900

    :zap: denom change

--- app/config.go
+++ app/config.go
@@ -5,7 +5,7 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 func init() {
 	// Set bond denom
 
-	sdk.DefaultBondDenom = "LWP"
+	sdk.DefaultBondDenom = "ulwp"
 
 	// Set address prefixes
 	accountPubKeyPrefix := AccountAddressPrefix + "pub"
