15dfc7d7b171c74a6d4a67cfbbd45ece1b84d69a
Top-Yone <thickeyes@gmail.com>
2026-04-06 20:07:52 +0900

    :zap: set default MinGasPrices to 0.025ulwp

--- cmd/lumiwave-protocold/cmd/config.go
+++ cmd/lumiwave-protocold/cmd/config.go
@@ -39,8 +39,7 @@ func initAppConfig() (string, interface{}) {
 	// - if you set srvCfg.MinGasPrices non-empty, validators CAN tweak their
 	//   own app.toml to override, or use this default value.
 	//
-	// In tests, we set the min gas prices to 0.
-	// srvCfg.MinGasPrices = "0stake"
+	srvCfg.MinGasPrices = "0.025ulwp"
 
 	customAppConfig := CustomAppConfig{
 		Config: *srvCfg,
